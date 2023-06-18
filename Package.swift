767fea93d07467ac3d4a13ff2639580360a943f6
Michael <dev@rifigy.app>
2023-06-17 20:59:12 -0400

    added files

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "MusicBrainzKit",
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v15), .watchOS(.v10), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MusicBrainzKit",
             targets: ["MusicBrainzKit"]),
     ],
+    dependencies: [
+        .package(path: "../DrillURL")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MusicBrainzKit"),
+            name: "MusicBrainzKit",
+            dependencies: [
+                .product(name: "DrillURL", package: "DrillURL")
+            ]),
         .testTarget(
             name: "MusicBrainzKitTests",
             dependencies: ["MusicBrainzKit"]),
---
+++ Sources/MusicBrainzKit/Artist/MBArtist.swift
@@ -0,0 +1,40 @@
+
+
+
+public struct MBArtist: Decodable {
+    public let id, name, sortName, typeID: String
+    public let type, disambiguation: String
+    public let gender, genderID: String?
+    public let country: String
+    public let area, beginArea, endArea: MBArea?
+    public let lifeSpan: MBLifespan?
+    public let isnis: [String]
+    public let aliases: [MBAlias]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case sortName = "sort-name"
+        case typeID = "type-id"
+        case type, disambiguation, gender
+        case genderID = "gender-id"
+        case country, area
+        case beginArea = "begin-area"
+        case endArea = "end-area"
+        case lifeSpan = "life-span"
+        case isnis, aliases
+    }
+}
+
+// MARK: - Alias
+public struct MBAlias: Decodable {
+    let name: String
+    let sortName: String
+    let typeID, locale, type: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case sortName = "sort-name"
+        case typeID = "type-id"
+        case locale, type
+    }
+}
