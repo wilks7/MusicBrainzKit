ebebf3dae2f1bd60c8aa036a81b8e18adfac7440
wilks7 <wilks7@icloud.com>
2023-06-20 18:10:33 -0400

    added dict to MBQuery

--- Sources/MusicBrainzKit/Protocols/MBQuery.swift
+++ Sources/MusicBrainzKit/Protocols/MBQuery.swift
@@ -11,7 +11,17 @@ public protocol MBQuery: Encodable {
     associatedtype Include: Includes
 }
 
+extension [String:String]:MBQuery {
+    public typealias Include = MiscIncludes
+}
+
 public struct DefaultQuery: MBQuery {
     public var id: String?
+    public var artistName: String?
+        
+    public init(id: String? = nil, artistName: String? = nil) {
+        self.id = id
+        self.artistName = artistName
+    }
     public typealias Include = RelationsIncludes
 }
